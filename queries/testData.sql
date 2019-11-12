6f50af52ec7c6fae8e0a6c883b8d93442d0fb815
farisally23 <55149883+farisally23@users.noreply.github.com>
2019-11-12 10:43:38 -0500

    Update testData.sql

--- queries/testData.sql
+++ queries/testData.sql
@@ -1,1 +1,99 @@
+﻿INSERT INTO a2.country VALUES (1, 'Canada');
+INSERT INTO a2.country VALUES (2, 'United States');
+INSERT INTO a2.country VALUES (3, 'Brazil');
+INSERT INTO a2.country VALUES (4, 'Japan');
+INSERT INTO a2.country VALUES (5, 'Jamaica');
 
+INSERT INTO a2.player VALUES (1, 'Faris Ally', 1, 1); 
+INSERT INTO a2.player VALUES (2, 'Kawhi Leonard', 4, 5) ;
+INSERT INTO a2.player VALUES (3, 'LeBron James', 7, 5) ;
+INSERT INTO a2.player VALUES (4, 'Steady Freddy', 3, 2) ;
+INSERT INTO a2.player VALUES (5, 'John Chang', 6, 4) ;
+INSERT INTO a2.player VALUES (6, 'Joe Mama', 5, 3) ;
+INSERT INTO a2.player VALUES (7, 'Pascal Siakim', 2, 1) ;
+
+INSERT INTO a2.tournament VALUES (1, 'Tourney A', 1);
+INSERT INTO a2.tournament VALUES (2, 'World Championship', 2);
+INSERT INTO a2.tournament VALUES (3, 'Super Cup', 3);
+INSERT INTO a2.tournament VALUES (4, 'Ternee', 4);
+INSERT INTO a2.tournament VALUES (5, 'Big Tournament', 5);
+INSERT INTO a2.tournament VALUES (6, 'Canada National', 1);
+
+INSERT INTO a2.champion VALUES (1, 2018, 4);
+INSERT INTO a2.champion VALUES (1, 2018, 3);
+INSERT INTO a2.champion VALUES (1, 2018, 5);
+INSERT INTO a2.champion VALUES (1, 2019, 1);
+INSERT INTO a2.champion VALUES (1, 2019, 2);
+INSERT INTO a2.champion VALUES (1, 2015, 6);
+INSERT INTO a2.champion VALUES (2, 2019, 5);
+INSERT INTO a2.champion VALUES (2, 2018, 6);
+INSERT INTO a2.champion VALUES (7, 2019, 3);
+INSERT INTO a2.champion VALUES (3, 2019, 4);
+
+
+
+INSERT INTO a2.court VALUES (1, 'Court1', 100, 1);
+INSERT INTO a2.court VALUES (2, 'Court2', 100, 1);
+INSERT INTO a2.court VALUES (3, 'Court3', 50, 2);
+INSERT INTO a2.court VALUES (4, 'Court4', 50, 2);
+INSERT INTO a2.court VALUES (5, 'Court5', 200, 3);
+INSERT INTO a2.court VALUES (6, 'Court6', 200, 3);
+INSERT INTO a2.court VALUES (7, 'Court7', 25, 4);
+INSERT INTO a2.court VALUES (8, 'Court8', 25, 4);
+INSERT INTO a2.court VALUES (9, 'Court9', 500, 5);
+INSERT INTO a2.court VALUES (10, 'Court10', 500, 5);
+INSERT INTO a2.court VALUES (11, 'Court11', 150, 5);
+INSERT INTO a2.court VALUES (12, 'Court12', 150, 5);
+
+INSERT INTO a2.record VALUES (1, 2011, 25, 0);
+INSERT INTO a2.record VALUES (1, 2012, 25, 0);
+INSERT INTO a2.record VALUES (1, 2013, 25, 0);
+INSERT INTO a2.record VALUES (1, 2014, 25, 0);
+
+INSERT INTO a2.record VALUES (2, 2011, 1, 24);
+INSERT INTO a2.record VALUES (2, 2012, 13, 12);
+INSERT INTO a2.record VALUES (2, 2013, 15, 10);
+INSERT INTO a2.record VALUES (2, 2014, 16, 9);
+
+INSERT INTO a2.record VALUES (3, 2011, 3, 22);
+INSERT INTO a2.record VALUES (3, 2012, 10, 15);
+INSERT INTO a2.record VALUES (3, 2013, 14, 11);
+INSERT INTO a2.record VALUES (3, 2014, 25, 0);
+
+INSERT INTO a2.record VALUES (4, 2011, 2, 23);
+INSERT INTO a2.record VALUES (4, 2012, 7, 18);
+INSERT INTO a2.record VALUES (4, 2013, 8, 17);
+INSERT INTO a2.record VALUES (4, 2014, 22, 3);
+
+INSERT INTO a2.record VALUES (5, 2011, 1, 24);
+INSERT INTO a2.record VALUES (5, 2012, 18, 7);
+INSERT INTO a2.record VALUES (5, 2013, 6, 19);
+INSERT INTO a2.record VALUES (5, 2014, 25, 0);
+
+INSERT INTO a2.record VALUES (6, 2011, 14, 11);
+INSERT INTO a2.record VALUES (6, 2012, 5, 20);
+INSERT INTO a2.record VALUES (6, 2013, 7, 18);
+INSERT INTO a2.record VALUES (6, 2014, 2, 23);
+
+INSERT INTO a2.record VALUES (7, 2011, 20, 5);
+INSERT INTO a2.record VALUES (7, 2012, 23, 2);
+INSERT INTO a2.record VALUES (7, 2013, 19, 6);
+INSERT INTO a2.record VALUES (7, 2014, 3, 22);
+
+INSERT INTO a2.event VALUES (1, 2014, 1, 1, 5, 250);
+INSERT INTO a2.event VALUES (2, 2014, 1, 1, 5, 250);
+INSERT INTO a2.event VALUES (3, 2014, 2, 1, 6, 250);
+INSERT INTO a2.event VALUES (4, 2014, 1, 3, 4, 200);
+INSERT INTO a2.event VALUES (5, 2014, 1, 3, 5, 200);
+INSERT INTO a2.event VALUES (6, 2014, 2, 3, 6, 200);
+INSERT INTO a2.event VALUES (7, 2014, 2, 1, 3, 200);
+INSERT INTO a2.event VALUES (8, 2014, 1, 2, 7, 100);
+INSERT INTO a2.event VALUES (9, 2014, 1, 2, 4, 100);
+INSERT INTO a2.event VALUES (10, 2014, 2, 2, 5, 100);
+INSERT INTO a2.event VALUES (11, 2014, 2, 2, 6, 100);
+INSERT INTO a2.event VALUES (12, 2014, 1, 4, 7, 200);
+INSERT INTO a2.event VALUES (13, 2014, 1, 4, 5, 200);
+INSERT INTO a2.event VALUES (14, 2014, 2, 4, 5, 200);
+INSERT INTO a2.event VALUES (15, 2014, 2, 4, 6, 200);
+INSERT INTO a2.event VALUES (16, 2011, 2, 4, 6, 100);
+INSERT INTO a2.event VALUES (17, 2010, 1, 1, 7, 200);
